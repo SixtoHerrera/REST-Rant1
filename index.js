bfe28796e91cf877995b31b943f5485d8ae51b8f
sixtoherrera <sixto.herrera@continental.com>
2023-11-12 20:39:26 -0600

    debugged again and working

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.use('/places', require('./controllers/places'))
+//app.use('/places', require('./controllers/places'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/places', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      res.render('places/index',{places})
   })
   
 
