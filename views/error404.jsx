305e47789f894310250e5319064e8437d591465b
sixtoherrera <sixto.herrera@continental.com>
2023-11-14 16:36:26 -0600

    bootstrap grid added to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,11 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="/images/cat.jpg" alt="cat image"/>
+      <div>
+        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
+      </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,25 @@
+
 const React = require('react')
 const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+    <h1>HOME</h1>
+    <div>
+    <img src="/images/2023.jpg" alt="2023 image"/>
+      <div>
+        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
+
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
