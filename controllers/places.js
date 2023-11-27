ed930a69080df1a9e3b59490470fcd85c8df983a
sixtoherrera <sixto.herrera@continental.com>
2023-11-26 23:25:53 -0600

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,8 @@
 const router = require('express').Router()
+const places = require('../models/places')
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 // GET /places
 app.get('/', (req, res) => {
@@ -26,8 +30,22 @@ app.get('/', (req, res) => {
   })
   router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
+  
+  
   
 
 module.exports = router
