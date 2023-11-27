aa543aa5fceb5ae9f09a31fad2cbbd493bd919ef
sixtoherrera <sixto.herrera@continental.com>
2023-11-26 22:56:34 -0600

    troubleshooting part 5

--- views/home.jsx
+++ views/home.jsx
@@ -1,4 +1,3 @@
-
 const React = require('react')
 const Def = require('./default')
 
@@ -6,17 +5,17 @@ function home () {
     return (
         <Def>
             <main>
-    <h1>REST-rant</h1>
-    <div>
-    <img src="/images/2023.jpg" alt="2023 image"/>
-      <div>
-        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-      </div>
-    </div>
-    <a href="/places">
-        <button className="btn-primary">Places Page</button>
-    </a>
-</main>
+              <h1>HOME</h1>
+              <div>
+                <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                <div>
+                  Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+              <a href="/places">
+                  <button className="btn-primary">Places Page</button>
+              </a>
+            </main>
 
         </Def>
     )
