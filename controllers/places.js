837ad9048e9af9da7fcf22f1046656bb7d32781a
sixtoherrera <sixto.herrera@continental.com>
2023-11-27 00:19:24 -0600

    debugged

--- controllers/places.js
+++ controllers/places.js
@@ -6,25 +6,13 @@ router.get('/', (req, res) => {
 
 // GET /places
 app.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
-  }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: '/images/coffee-cat.jpg'
-  }]
-   
+
+   res.render('places/index', { places/new })
       
     
   })
   
-  res.render('places/index', { places/new })
+  
   router.get('/new', (req, res) => {
     res.render('places/new')
   })
